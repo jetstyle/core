9cdb6da877a5fd6e528da931d8790d5fcc5c2d5a
lucky <lucky@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-03-21 06:30:19 +0000

    * f
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@75 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/classes/controllers/BasicLoginPage.php
+++ core/core.nop/classes/controllers/BasicLoginPage.php
@@ -7,6 +7,14 @@
 $this->UseClass("controllers/BasicPage");
 class BasicLoginPage extends BasicPage
 {
+	var $tpl_login_sitemap = 'login';
+	var $tpl_login_form = '_login/form.html';
+	var $tpl_login_form_store_to = '_maincolumn';
+
+	var $tpl_success_sitemap = 'login';
+	var $tpl_success_form = '_login/user.html';
+	var $tpl_success_form_store_to = '_maincolumn';
+	var $tpl_success_data = '*';
 
 	function Handle()
 	{
@@ -15,15 +23,17 @@ class BasicLoginPage extends BasicPage
 		#$this->rh->tpl->set('*', $this->rh->data);
 		if ($this->rh->principal->Identify(PRINCIPAL_NO_REDIRECT) > PRINCIPAL_AUTH)
 		{
-			$this->rh->tpl->parse('_login/form.html', '_maincolumn');
-			$this->rh->site_map_path = 'login';
+			$this->rh->tpl->parse($this->tpl_login_form, 
+				$this->tpl_login_form_store_to);
+			$this->rh->site_map_path = $this->tpl_login_sitemap;
 		}
 		else
 		{
 			//var_dump($this->rh->principal->Security('role', 'user'));
-			$this->rh->tpl->set('*', $this->rh->principal->data);
-			$this->rh->tpl->parse('_login/user.html', '_maincolumn');
-			$this->rh->site_map_path = 'login';
+			$this->rh->tpl->set($this->tpl_success_data, $this->rh->principal->data);
+			$this->rh->tpl->parse($this->tpl_success_form, 
+				$this->tpl_success_form_store_to);	
+			$this->rh->site_map_path = $this->tpl_success_sitemap;
 		}
 	}
 
