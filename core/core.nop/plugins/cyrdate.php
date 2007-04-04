7d19d7b211f484e57e37508a6581b11cdf183de9
lucky <lucky@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-04-04 04:39:06 +0000

    * plugins/cyrdate     * accept CyrDate object as 'date' argument
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@120 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/cyrdate.php
+++ core/core.nop/plugins/cyrdate.php
@@ -13,8 +13,16 @@ $date = isset($params['date']) ? $params['date'] : $params['_'];
 $ifmt = isset($params['iformat']) ? $params['iformat'] : '%d.%m.%Y';
 $ofmt = isset($params['oformat']) ? $params['oformat'] : $params[0];
 
-$rh->useClass('CyrDate');
-$d = CyrDate::newFromStr($ifmt, $date);
+if (is_object($date))
+{
+	$d =& $date;
+}
+else
+{
+	$rh->useClass('CyrDate');
+	$d =& CyrDate::newFromStr($ifmt, $date);
+}
+
 if (isset($d))
 {
 	$d->ctx =& $rh;
