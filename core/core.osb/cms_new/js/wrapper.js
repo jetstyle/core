9d797a352a8b6f34701628173ecbc4f1f0ec6ab3
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-04-28 04:18:47 +0000

    - removed many fucking deprecated stuff
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@183 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.osb/cms_new/js/wrapper.js
+++
@@ -1,5 +0,0 @@
-function WrapperSwitch(id,visible){
-  document.getElementById('module_div_opened_'+id).className = visible ? 'visible' : 'invisible'; 
-  document.getElementById('module_div_closed_'+id).className = !visible ? 'visible' : 'invisible'; 
-  document.cookie = "c"+id+"=" + ( visible ? 0 : 1 );
-}
