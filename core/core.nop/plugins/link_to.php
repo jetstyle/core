f79c7667ffe8d862c6e176757e98530c5a5124aa
lucky <lucky@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-05-12 15:54:20 +0000

    * plugins/link_to: initial support for ContentType
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@211 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/link_to.php
+++ core/core.nop/plugins/link_to.php
@@ -1,37 +1,72 @@
 <?php
 
 /**		
- *	{{!link_to class=News item=*}}	     
+ * # ссылка на item на странице 'News'
+ *	{{!link_to class='News' item=*}}	     
+ *
+ * # ссылка на страницу 'News'
+ *	{{!link_to class='News'}}
+ *	{{!link_to 'News'}}
+ *
+ * # ссылка на item
+ * # в этом случае, item должен содержать 
+ * # поле 'ContentType' -- тип объекта
+ * # или поле 'link' -- link на страницу FIXME: пока как совместимость -- 
+ * #	  потом удалить нафик
+ *	{{!link_to item=*}}	     
+ *	{{!link_to *}}	     
  *
  *	создает относительные ссылки, относительно конря сайта
  *
  *	path/to/page
  *
  */
-/* если массива news нету в шаблонном дамайне, то возьмём из фикстур :P */
-
-$class = $params['class']?$params['class']:$params[0]; // ключ
-//можно без each= а сразу, {{!url_to News/NewsItem}}	     
-
-$item = $params['item']?$params['item']:$params[1]; // ключ
-//можно {{!url_to News #NewsItem}}	     
-
-$clss = explode('/', $class, 2);
-if (count($clss) == 2)
-{
-	list($page_cls, $item_cls) = $clss;
-}
-else
-{
-	$page_cls = $class;
-	$item_cls = NULL;
-}
 
 $url = NULL;
-$p =& $rh->getPageByContentType($page_cls);
-if ($p)
+
+$class = $params['class']?$params['class']:$params[0]; // тип
+$item =  $params['item']?$params['item']:$params[1]; // значение
+
+
+if (isset($class) && !is_scalar($class))
+	// первым аргументом передали item
 {
-	$url = $p->url_to($item_cls, $item);
+	$item = $class;
+	unset($class);
+}
+
+if (!isset($class))
+{
+	// lucky: FIXME -- $item не факт что массив
+	if (isset($item['href'])) 
+	{
+		$url = $item['href'];
+	} else 
+	if (isset($item['ContentType']))
+	{
+		$class = $item['ContentType'];
+	}
+}
+
+
+if (isset($class))
+{
+	$clss = explode('/', $class, 2);
+	if (count($clss) == 2)
+	{
+		list($page_cls, $item_cls) = $clss;
+	}
+	else
+	{
+		$page_cls = $class;
+		$item_cls = NULL;
+	}
+
+	$p =& $rh->getPageByContentType($page_cls);
+	if ($p)
+	{
+		$url = $p->url_to($item_cls, $item);
+	}
 }
 
 if (isset($url)) echo $url;
