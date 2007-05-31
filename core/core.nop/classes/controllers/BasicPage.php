6d22e0e216b46afa96abe78823a725d6eaa35397
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-05-31 05:15:21 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/trunk@236 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/classes/controllers/BasicPage.php
+++ core/core.nop/classes/controllers/BasicPage.php
@@ -241,6 +241,8 @@ class BasicPage extends Controller
 		// (первого из элементов)
 		$this->rh->tpl->set('PAGE', $this);
 		$this->rh->tpl->set('name', $this->title);
+		$this->rh->tpl->set('meta_keywords', $this->meta_keywords);
+		$this->rh->tpl->set('meta_description', $this->meta_description);
 		$this->notifyOnRend();
 	}
 
