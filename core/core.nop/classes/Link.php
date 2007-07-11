5502179332346100daf9f7a376b7601ea19707d7
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-07-11 16:52:14 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/trunk@258 fa5afafc-1ec3-db11-ae4b-00804824780f

---
+++ core/core.nop/classes/Link.php
@@ -0,0 +1,28 @@
+<?php
+
+class Link
+{
+	var $rh = null;
+	var $allowed_proto = array();
+	
+	function Link(&$rh)
+	{
+		$this->rh = &$rh;
+		$this->allowed_proto = explode(',', $this->rh->link_allowed_protocols);
+	}
+	
+	function formatLink($val)
+	{
+		foreach($this->allowed_proto AS $r)
+		{
+			if(!(strpos($val, $r) === false))
+			{
+				return $val;
+			}
+		}
+		return $this->rh->ri->Href(ltrim($val, '/'));		
+	}
+	
+}
+
+?>
