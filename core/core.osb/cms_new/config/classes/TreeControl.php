14de397ff6ca994819291dabe42d3c3010d4a6ff
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2007-08-20 09:28:51 +0000

    ban buttons for root
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@290 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.osb/cms_new/config/classes/TreeControl.php
+++ core/core.osb/cms_new/config/classes/TreeControl.php
@@ -117,6 +117,15 @@ class TreeControl extends DBDataEditTree  {
 				break;
 
 			default:
+			
+				$tpl =& $this->rh->tpl;
+    
+			    $template_ban = $this->template_control.':buttons_ban';
+			    //корень
+			    $tpl->Assign('_id',0);
+			    $tpl->Assign('_array',implode(',',array(1,1,1)));
+			    $tpl->Parse( $template_ban, 'buttons_ban', true );
+			
 				//  
 				$this->_Handle();
 				//     
