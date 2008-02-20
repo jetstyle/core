995112e92df88614d0ee765f56257af676c3bd0a
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-02-20 06:05:52 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/trunk@400 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.osb/cms_new/config/classes/Trash.php
+++ core/core.osb/cms_new/config/classes/Trash.php
@@ -2,7 +2,7 @@
 /*
     Trash( $rh ) -- Класс для работы с корзиной
       $rh -- ссылка на $rh
-      
+
   ---------
   * Delete( $table_name, $item_id, $module_title, $item_title, $view_link ) --
             удаляет запись в корзину либо, если эта запись уже помещена в корзину, удаляет её окончательно
@@ -11,20 +11,20 @@
       - $module_title -- название модуля, элемент которого удаляем
       - $item_title -- название элемента, который удаляем
       - $view_link -- ссылка на просмотр данного элемента
-  
-  * ToTrash( $table_name, $item_id, $module_title, $item_title, $view_link ) -- 
+
+  * ToTrash( $table_name, $item_id, $module_title, $item_title, $view_link ) --
             перемещает указанную запись в корзину, параметры  такие же, как у Delete()
-  
+
   * FromTrash( $table_name, $item_id ) -- восстанавливает запись из корзины
       - $table_name -- имя таблицы в БД, запись из которой нужно "удалить"
       - $item_id -- ID записи в этой таблице
-  
+
   * Erase( $table_name, $item_id ) -- удаляет запись окончательно
         параметры такие же, как у FromTrash()
-  
+
   * _ReadTrashRecord( $table_name, $item_id ) -- возвращает синтетическую запись из таблиц мусора для указанной записи
         параметры такие же, как у FromTrash()
-  
+
   ---------
 Концепция:
 "перемещение в корзину" - исходная запись помечается как '_state=2',
@@ -33,28 +33,28 @@
     удаляются соответствующие записи из таблиц мусора
 "окончательное удаление" - исходная запись удаляется из своей таблицы
     удаляются соответствующие записи из таблиц мусора
-  
+
 =============================================================== v.1 (Zharik)
 */
-  
+
 // поместить в конзину
 define ('OSB_TRASH_WASTE', 1);
 // удалить навсегда
 define ('OSB_TRASH_ERASE', 2);
 
 class Trash {
-  
+
   var $rh;
   var $table_trash;
   var $table_trash_tables;
   var $table_id_field="id";
-  
+
   function Trash(&$rh){
     $this->rh =& $rh;
     $this->table_trash = $rh->project_name.'_trash';
     $this->table_trash_tables = $rh->project_name.'_trash_tables';
   }
-  
+
   function Delete( $table_name, $item_id, $module_title, $item_title, $view_link )
   {
     $sql = "SELECT ".$this->table_id_field.", _state FROM $table_name WHERE ".$this->table_id_field."='$item_id'";
@@ -74,7 +74,7 @@ class Trash {
       return OSB_TRASH_ERASE;
     }
   }
-  
+
   function ToTrash( $table_name, $item_id, $module_title, $item_title, $view_link )
   {
     $db =& $this->rh->db;
@@ -82,7 +82,7 @@ class Trash {
     $db->execute("UPDATE $table_name SET _state=2 WHERE ".$this->table_id_field."='$item_id'");
     //вставляем запись в таблицу мусора
     $rs = $db->queryOne("SELECT id FROM ".$this->table_trash_tables." WHERE table_name='$table_name'");
-    
+
     if( $rs['id'] ) $table_id = $rs['id'];
     else
     {
@@ -98,17 +98,17 @@ class Trash {
     $sql .= " VALUES('$table_id','$item_id','$module_title','$item_title','$view_link')";
     $db->execute($sql);
   }
-  
+
   function FromTrash( $table_name, $item_id ){
     $db =& $this->rh->db;
     //читаем запись из таблицы мусора
     $r = $this->_ReadTrashRecord($table_name, $item_id);
     //снимаем пометку "удалён" в исходной таблице
-    $db->execute("UPDATE ".$r->table_name." SET _state=0 WHERE id='".$r->item_id."'");
+    $db->execute("UPDATE ".$r->table_name." SET _state=0 WHERE ".$this->table_id_field."='".$r->item_id."'");
     //удаляем запись из таблицы мусора
     $db->execute("DELETE FROM ".$this->table_trash." WHERE id='".$r->trash_id."'");
   }
-  
+
   function Erase( $table_name, $item_id )
   {
     $db =& $this->rh->db;
@@ -123,7 +123,7 @@ class Trash {
     	$db->execute("DELETE FROM ".$this->table_trash." WHERE id='".$r->trash_id."'");
 	}
   }
-  
+
   function _ReadTrashRecord( $table_name, $item_id ){
     $sql = "SELECT tr.id as trash_id, table_id, table_name, item_id";
     $sql .= " FROM ".$this->table_trash." as tr, ".$this->table_trash_tables." as tb ";
@@ -134,5 +134,5 @@ class Trash {
     return $this->rh->db->getObject();
   }
 }
-  
+
 ?>
