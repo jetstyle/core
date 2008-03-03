c5e26768dcd17ab43968afe3e0d93ffb8dd32f2a
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-03-03 08:05:06 +0000

    * QUICKSTART-107
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@413 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/classes/Debug.php
+++ core/core.nop/classes/Debug.php
@@ -5,11 +5,16 @@
  */
 class Debug
 {
-	static protected $log;
+	static protected $log = array();
 	static protected $_milestone;
 	static protected $milestone;
+	static protected $prefix = "<div class='debug-div' id='debug-div'>Trace log:<ul><li>";
+	static protected $separator = "</li><li>";
+	static protected $postfix = "</li></ul></div><div class='debug-cont' id='debug-cont'></div>";
+	static protected $canGetMemoryUsage = false;
 	static protected $mark = array();
-
+	static protected $inc = 1;
+	
 	/**
     * Инициализация
     *
@@ -19,6 +24,7 @@ class Debug
 		self::$milestone = self::_getmicrotime();
 		self::$_milestone = self::$milestone;
 		self::$mark['auto'] = self::$milestone;
+		self::$canGetMemoryUsage = function_exists('memory_get_usage');
 		self::trace("<b>log started.</b>");
 	}
 
@@ -40,34 +46,38 @@ class Debug
     * @param $postfix
     * @return HTML
     */
-	static public function getHtml( $prefix="<div>Trace log:</div><ul><li>", $separator="</li><li>", $postfix="</li></ul><div id='debug_div' class='debug_div'></div>" )
+	static public function getHtml()
 	{
 		$out = '';
-		self::trace( "<b>log flushed.</b>");
-		$out.=$prefix;
-		$f=0;
-		foreach (self::$log as $item)
+		self::trace( "<b>log flushed</b>");
+		$out .= self::getStyles();
+		$out .= self::$prefix;
+		
+		foreach (self::$log AS $item)
 		{
-			if (!$f) $f=1; else $out.=$separator;
-			$out.=$item;
+			$out .= self::constructHtmlItem($item);
+			$out .= self::$separator;
 		}
-		$out.=$postfix;
+		
+		$out .= self::$postfix;
+		$out .= self::getJavascript();
 		self::$log = array();
 		return $out;
 	}
-
+	
+	
 	/**
 	 * Добавление записи в лог
 	 *
-	 * @param string $what
+	 * @param string $title
 	 * @param string $category категория записи
 	 * @param string $label отметка, поставленная функцией mark(), от нее будет считаться время выполнения
 	 */
-	static public function trace( $what, $category = null, $label = null)
+	static public function trace( $title, $category = null, $label = null, $text = '')
 	{
 		$m = self::_getmicrotime();
 		$diff = $m - self::$_milestone;
-		if($label)	
+		if ($label)	
 		{
 			$diff1 = $m - self::$mark[$label];
 			unset(self::$mark[$label]);
@@ -77,11 +87,18 @@ class Debug
 			$diff1 = $m - self::$mark['auto'];
 		}
 
-		if (function_exists('memory_get_usage'))
+		if (self::$canGetMemoryUsage)
 		{
 			$memory = number_format((memory_get_usage() / 1024));
 		}
-		self::$log[] = sprintf("[%0.4f] ",$diff).sprintf("[%0.4f] ",$diff1).( $memory ? " [$memory kb] " : "").$what;
+		
+		self::$log[] = array(
+			'time' => $diff,
+			'diffTime' => $diff1,
+			'memory' => $memory,
+			'title' => $title,
+			'text' => $text,
+		);
 		self::$mark['auto'] = $m;
 	}
 
@@ -103,6 +120,115 @@ class Debug
 		header("Content-Type: text/html; charset=windows-1251");
 		if ($flush) echo self::$getHtml();
 		die("К сожалению, произошла ошибка.");
+	}
+	
+	static public function setPrefix($d)
+	{
+		self::$prefix = $d;
+	}
+	
+	static public function setSeparator($d)
+	{
+		self::$separator = $d;
+	}
+	
+	static public function setPostfix($d)
+	{
+		self::$postfix($d);
+	}
+	
+	static protected function constructHtmlItem($item)
+	{
+		$out = '';
+		
+		$out .= sprintf("[%0.4f] ",$item['time']);
+		$out .= sprintf("[%0.4f] ",$item['diffTime']);
+		
+		if($item['memory'])
+		{
+			$out .= '['.$item['memory'].' kb]';
+		}
+		
+		$out .= '&nbsp;&nbsp;';
+		
+		if($item['text'])
+		{
+			$out .= '<span class="debug-clickable" onclick="JSDebug.show(this)" id="'.self::getId().'">'.$item['title'].'</span><span style="display: none;">'.$item['text'].'</span>';
+		}
+		else
+		{
+			$out .= $item['title'];
+		}
+		
+		return $out;
+	}
+	
+	static protected function getId()
+	{
+		return 'debug'.self::$inc++;
+	}
+	
+	static protected function getStyles()
+	{
+		$txt = '<style type="text/css">
+			.debug-div {font-size: 150%;}
+			.debug-clickable {cursor: pointer; }
+			.debug-cont {position: absolute; background-color: #CACACA; border: 1px solid black; padding: 5px; font-size: 150%;}
+			.debug-cont td {border-right: 1px solid grey; border-bottom: 1px solid grey; padding: 3px;}
+		</style>';
+		
+		return $txt;
+	}
+	
+	static protected function getJavascript()
+	{
+		$txt = '<script language="javascript">
+					JSDebug = function(){
+						this.cont = document.getElementById("debug-cont");
+						this.selectedId = null;
+					}
+					JSDebug.prototype.show = function(el)
+					{
+						if(el && el.nextSibling)
+						{
+							JSDebug.showPopup(el, el.nextSibling.innerHTML);
+						}
+					}
+					JSDebug.prototype.showPopup = function(el, txt)
+					{
+						if(JSDebug.selectedId == el.id)
+						{
+							JSDebug.cont.style.display = "none";
+							JSDebug.selectedId = null;
+							return;
+						}
+						var c = JSDebug.getPosition(el);
+						JSDebug.cont.style.left = c.x + "px";
+						JSDebug.cont.style.top = (c.y + el.offsetHeight) + "px";
+						JSDebug.cont.innerHTML = txt;
+						JSDebug.cont.style.display = "";
+						JSDebug.selectedId = el.id;
+					}
+					JSDebug.prototype.getPosition = function(e)
+					{
+						var left = 0;
+						var top  = 0;
+						
+						while (e.offsetParent){
+							left += e.offsetLeft + (e.currentStyle?(parseInt(e.currentStyle.borderLeftWidth)).NaN0():0);
+							top  += e.offsetTop  + (e.currentStyle?(parseInt(e.currentStyle.borderTopWidth)).NaN0():0);
+							e     = e.offsetParent;
+						}
+				
+						left += e.offsetLeft + (e.currentStyle?(parseInt(e.currentStyle.borderLeftWidth)).NaN0():0);
+						top  += e.offsetTop  + (e.currentStyle?(parseInt(e.currentStyle.borderTopWidth)).NaN0():0);
+				
+						return {x:left, y:top};
+					}
+					var JSDebug = new JSDebug();
+				</script>';
+		
+		return $txt;
 	}
 }
 ?>
