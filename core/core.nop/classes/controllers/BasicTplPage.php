f4a7296ec9fd6251df6d5febbb36254d1329db2c
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-03-28 10:24:22 +0000

    * fixed DT-131 (menu in tpl handler)
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@445 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/classes/controllers/BasicTplPage.php
+++ core/core.nop/classes/controllers/BasicTplPage.php
@@ -7,10 +7,20 @@
 $this->UseClass("controllers/BasicPage");
 class BasicTplPage extends BasicPage
 {
+	var $plugins = array(
+		array('MenuPlugin', array(
+			'__aspect' => 'MainMenu',
+			'store_to' => 'menu', 
+			'level' => 2,
+			'depth' => 2,
+		)),
+	);
 
 	function handle()
 	{
         //например так
+        parent::handle();
+
         $this->rh->tpl->set('tpl', 'tpl');
         $this->rh->site_map_path= rtrim(implode("/", $this->params), "/");
 	}
