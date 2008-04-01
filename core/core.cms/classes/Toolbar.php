1b5f490519be14d881d1ec173536a51522a55cb5
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-04-01 16:03:18 +0000

    * check ACL
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@452 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.cms/classes/Toolbar.php
+++ core/core.cms/classes/Toolbar.php
@@ -36,44 +36,71 @@
  	 */
  	protected function load()
  	{
- 		if($result = $this->rh->db->execute("" .
+ 		$this->constructResult($this->getLoadResult());
+ 	}
+ 	
+ 	protected function getLoadResult()
+ 	{
+ 		return $this->rh->db->execute("" .
  				"SELECT id, title, href, _level, _parent " .
  				"FROM ??toolbar " .
  				"WHERE _state = 0 AND _level IN (1,2) " .
  				"ORDER BY _level ASC, _order ASC " .
- 		""))
+ 		"");
+ 	}
+ 	
+ 	protected function constructResult($result)
+ 	{
+ 		$module_name = 'do/'.$this->rh->getVar('module');
+ 		while($r = $this->rh->db->getRow($result))
  		{
-	 		$module_name = 'do/'.$this->rh->getVar('module');
-	 		while($r = $this->rh->db->getRow($result))
-	 		{
-	 			if($r['_level'] == 1)
-	 			{
-	 				$this->items[$r['id']] = $r;
-	 			}
-	 			else
-	 			{
-	 				if(!isset($this->subItems[$r['_parent']]))
-	 				{
-	 					$this->subItems[$r['_parent']] = array('id' => $r['_parent'], 'childs' => array());
-	 				}
-	 				$this->subItems[$r['_parent']]['childs'][$r['id']] = $r;
-	 			}
-	 			if($module_name == $r['href'])
-	 			{
-	 				if($this->items[$r['id']])
-	 				{
-	 					$this->items[$r['id']]['selected'] = true;
-	 					$this->rh->tpl->set('menu_selected', $r['id']);
-	 				}
-	 				else
-	 				{
-	 					$this->subItems[$r['_parent']]['childs'][$r['id']]['selected'] = true;
-	 					$this->subItems[$r['_parent']]['selected'] = true;
-	 					$this->items[$r['_parent']]['selected'] = true;
-	 					$this->rh->tpl->set('menu_selected', $r['_parent']);
-	 				}
-	 			}
-	 		}
+ 			$r['granted'] = $this->rh->prp->isGrantedTo($r['href']);
+ 			
+ 			
+ 			if($r['_level'] == 1)
+ 			{
+ 				$this->items[$r['id']] = $r;
+ 			}
+ 			else
+ 			{
+ 				if(!$r['granted'])
+ 				{
+ 					continue;
+ 				}
+ 				elseif($r['granted'] && $this->items[$r['_parent']])
+ 				{
+ 					$this->items[$r['_parent']]['granted'] = true;
+ 				}
+ 				
+ 				if(!isset($this->subItems[$r['_parent']]))
+ 				{
+ 					$this->subItems[$r['_parent']] = array('id' => $r['_parent'], 'childs' => array());
+ 				}
+ 				$this->subItems[$r['_parent']]['childs'][$r['id']] = $r;
+ 			}
+ 			if($module_name == $r['href'])
+ 			{
+ 				if($this->items[$r['id']])
+ 				{
+ 					$this->items[$r['id']]['selected'] = true;
+ 					$this->rh->tpl->set('menu_selected', $r['id']);
+ 				}
+ 				else
+ 				{
+ 					$this->subItems[$r['_parent']]['childs'][$r['id']]['selected'] = true;
+ 					$this->subItems[$r['_parent']]['selected'] = true;
+ 					$this->items[$r['_parent']]['selected'] = true;
+ 					$this->rh->tpl->set('menu_selected', $r['_parent']);
+ 				}
+ 			}
+ 		}
+ 		
+ 		foreach($this->items AS $k => $item)
+ 		{
+ 			if(!$item['granted'])
+ 			{
+ 				unset($this->subItems[$item['id']], $this->items[$k]);
+ 			}
  		}
  	}
  	 	
