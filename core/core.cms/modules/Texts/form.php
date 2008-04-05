54015bfd25a7c10d8c61d77e16260fd669fbfc0b
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-04-05 08:29:05 +0000

    + texts
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@455 fa5afafc-1ec3-db11-ae4b-00804824780f

---
+++ core/core.cms/modules/Texts/form.php
@@ -0,0 +1,15 @@
+<?
+	$this->class_name = 'TextsForm';
+	$this->table_name = $this->rh->project_name.'_texts';
+	$this->SELECT_FIELDS = array('id','title','text','_state','_supertag','type');
+	$this->RENDER = array(
+		array('_state','checkbox'),
+		array('type','select', array('текст с оформлением','плоский текст') ),
+	);
+	/*
+	$this->PRE_FILTERS = array(
+		'typo_outlink' => array('title','text'),
+	);
+	*/
+	
+?>
