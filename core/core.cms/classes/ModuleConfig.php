0a2b10510097a6ad8b3de8168329f962d6cd40dd
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-04-07 16:35:32 +0000

    * fixed QUICKSTART-85
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@462 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.cms/classes/ModuleConfig.php
+++ core/core.cms/classes/ModuleConfig.php
@@ -86,6 +86,13 @@ class ModuleConfig {
 	{
 		$rh =& $this->rh;
 		//грузим класс
+		$first_dir_parts = explode("/", $this->rh->DIRS[0]);
+
+		//чтобы не добавлять по нескольку раз, тк мы здесь оказываемся 3 раза
+		$module_dir = $this->rh->DIRS[0].$this->handlers_type.'/'.$this->module_name.'/';
+		if ( $this->module_name != $first_dir_parts[ count($first_dir_parts)-2 ] )
+			 array_unshift($this->rh->DIRS, $module_dir);
+		
 		if( $this->get_class_here )
 		{
 			require_once( $this->rh->FindScript( $this->handlers_type, $this->module_name.'/'.$this->class_name ) );
