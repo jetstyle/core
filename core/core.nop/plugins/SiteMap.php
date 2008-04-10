99b7435d2ef6a290d6d92348dba0c02f79c0a4fb
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-04-10 04:55:59 +0000

    * for verstaks
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@474 fa5afafc-1ec3-db11-ae4b-00804824780f

---
+++ core/core.nop/plugins/SiteMap.php
@@ -0,0 +1,14 @@
+<?php
+#$base_url =$tpl->rh->base_url.'tests/templates/'; 
+$base_url =$tpl->rh->base_url;
+
+echo '<ul>';
+foreach( $tpl->rh->site_map as $key => $map ) if ($key){  
+  $href = $base_url.$key;
+  echo ( '<li><a href="'.$href.'">'.$tpl->rh->site_map[$key]['name'].'</a></li>');
+}
+echo '</ul>';
+
+#echo ( '<small><a href="'.$tpl->rh->base_url.'tests/templates_test/'.'">Проверить все шаблоны на ошибки парсинга</a></small>');
+
+?>
