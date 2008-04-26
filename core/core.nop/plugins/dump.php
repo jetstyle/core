03385b86186eec1de3ec90225fec31263ab800be
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-04-26 09:05:07 +0000

    * fixed |dump
    git-svn-id: svn://svn.jetstyle.ru/core/branch/02@493 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/dump.php
+++ core/core.nop/plugins/dump.php
@@ -15,7 +15,10 @@ $item = $params['item']?$params['item']:( $params['_'] ? $params['_'] : $params[
 
 
 echo '<pre>';
-print_r($item);
+if (is_object($item) && get_class($item)=='ResultSet')
+	echo $item;
+else 
+	print_r($item);
 echo '</pre>';
 
 ?>
