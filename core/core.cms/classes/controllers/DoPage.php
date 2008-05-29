abe4ab5a3291f294a62c41d876626a1df48d9fdc
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-05-29 09:33:25 +0000

    redirect after cms login for QUICKSTART-94
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@573 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.cms/classes/controllers/DoPage.php
+++ core/core.cms/classes/controllers/DoPage.php
@@ -10,7 +10,7 @@ class DoPage extends BasicPage
 	var $plugins = array(
 		array('ToolbarPlugin', array(
 			'__aspect' => 'Toolbar',
-			'store_to' => 'toolbar', 
+			'store_to' => 'toolbar',
 		)),
 	);
 
@@ -29,7 +29,11 @@ class DoPage extends BasicPage
 	{
 		if (!$this->rh->principal->isAuth())
 		{
-			$this->rh->redirect($this->rh->base_url.'login');
+			$redirectTo = $this->rh->base_url.'login'; //login page
+            $redirectTo .= '?retpath='; //path to return there
+            $redirectTo .= $_SERVER['HTTPS'] ? 'https://' : 'http://';
+            $redirectTo .= $_SERVER['SERVER_NAME'].$this->rh->ri->hrefPlus('');
+			$this->rh->redirect($redirectTo);
 		}
 
 		parent::handle();
@@ -39,7 +43,7 @@ class DoPage extends BasicPage
 	{
 		$this->rh->redirect($this->rh->base_url.'start');
 	}
-	
+
 	function handle_default($config)
 	{
 		//первый конфиг
@@ -47,15 +51,15 @@ class DoPage extends BasicPage
 		$moduleConstructor =& new ModuleConstructor($this->rh);
 		$moduleConstructor->initialize($config['module']);
 		$this->rh->tpl->set('module_body', $moduleConstructor->proceed($config['mode']));
-		
-		
+
+
 		/*
 		$moduleConfig->read("defs");
 		if ($config['mode'])
 		{
 			$moduleConfig->read( $config['mode'] );
 		}
-		
+
 		//основной модуль
 		$module = $moduleConfig->initModule();
 		$module->store_to = "module_body";
--- core/core.cms/classes/controllers/LoginPage.php
+++ core/core.cms/classes/controllers/LoginPage.php
@@ -3,24 +3,27 @@ $this->UseClass("controllers/BasicPage");
 class LoginPage extends BasicPage
 {
 	var $plugins = array();
-	
+
 	var $params_map = array(
 		array('default', array(NULL)),
 	);
-	
+
 	function handle_default($config)
 	{
 		if ($this->rh->principal->isAuth())
 		{
-			if ($this->rh->ri->get('logout')) 
+			if ($this->rh->ri->get('logout'))
 			{
 				$this->rh->principal->logout($this->rh->base_url.'login');
 			}
-			
-			$this->rh->redirect($this->rh->base_url.'start');
+
+			if ($_GET['retpath']) {            	$this->rh->redirect($_GET['retpath']);
+			} else {
+				$this->rh->redirect($this->rh->base_url.'start');
+			}
 		}
 		else
-		{			
+		{
 			$this->rh->site_map_path = 'login';
 		}
 	}
