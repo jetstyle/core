2ec9ff498e4799e5e7da0378af24f1e39d61ee23
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-06-11 10:31:42 +0000

    * QUICKSTART-215
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@582 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/cms_panel.php
+++ core/core.nop/plugins/cms_panel.php
@@ -1,6 +1,7 @@
 <?php
 
 	if($rh->principal->isAuth()) {		$tpl->set('oce_on',$rh->ri->get('oce') == 'on');
+		$tpl->set('cur_url',$rh->ri->hrefPlus('',''));
         return $tpl->parse('cms_panel.html');
 	} else return '';
 
