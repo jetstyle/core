dccfcbb88a293c80d4a450832a13eace77c19847
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-06-20 03:35:46 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@602 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.cms/classes/ModulePacker.php
+++ core/core.cms/classes/ModulePacker.php
@@ -6,16 +6,23 @@
  * Дампим структуру таблиц в .meta/structure.sql
  * Дампим данные таблиц в .meta/data.sql
  * Дампим строчку из таблицы Toolbar в .meta/toolbar.sql  
- * 
+ *
+ * @author lunatic lunatic@jetstyle.ru
+ * @created 19.06.2008
+ *  
  */
 
 class ModulePacker
 {
 	protected $rh;
+	protected $sqlDumper = null;
 	
 	public function __construct(&$rh)
 	{
 		$this->rh = &$rh;
+		$this->rh->useClass('ModuleConfig');
+		$this->rh->useClass('sql/SqlDump');
+		$this->sqlDumper = new SqlDump($this->rh);
 	}
 	
 	/**
@@ -43,16 +50,102 @@ class ModulePacker
 	
 	protected function packModule($module)
 	{
-		var_dump($module);
-		die();
+		$moduleDir = $this->rh->app_dir.'modules/'.$module['href'];
+		
+		if (!file_exists($moduleDir))
+		{
+			return false;
+		}
+		
+		$this->cleanUp($moduleDir);
+		$tables = $this->getTables($moduleDir);
+		
+		if (is_array($tables) && !empty($tables))
+		{
+			foreach ($tables AS $table)
+			{
+				$this->dumpStructure($table, $moduleDir);
+				$this->dumpData($table, $moduleDir);
+			}
+		}
+
+		$this->dumpModuleRow($module);
 	}
 	
+	/**
+	 * Дампим структуру таблицы в файл .meta/structure.sql
+	 *
+	 * @param string $tableName
+	 * @param string $moduleDir
+	 */
+	protected function dumpStructure($tableName, $moduleDir)
+	{
+		$this->sqlDumper->dumpStructure($tableName, $moduleDir.'/.meta/structure.sql');
+	}
+	
+	/**
+	 * Дампим данные таблицы в файл .meta/structure.sql
+	 *
+	 * @param string $tableName
+	 * @param string $moduleDir
+	 */
+	protected function dumpData($tableName, $moduleDir)
+	{
+		$this->sqlDumper->dumpData($tableName, $moduleDir.'/.meta/data.sql');
+	}
+	
+	protected function dumpModuleRow()
+	{
+		
+	}
+	
+	/**
+	 * Удаляем старые файлы 
+	 *
+	 * @param string $dir
+	 */
+	protected function cleanUp($dir)
+	{
+		@unlink($dir.'/.meta/structure.sql');
+		@unlink($dir.'/.meta/data.sql');
+		@unlink($dir.'/.meta/toolbar.sql');
+	}
+	
+	/**
+	 * Получаем имена всех таблиц, используемых в модуле
+	 *
+	 * @param string $moduleDir
+	 */
+	protected function getTables($moduleDir, $configName = 'defs')
+	{
+		$result = array();
+				
+		$config = new ModuleConfig($this->rh);
+		$config->read($moduleDir.'/'.$configName.'.php');
+		
+		$wrapped = $config->get('WRAPPED');
+		
+		if (is_array($wrapped) && !empty($wrapped))
+		{
+			foreach ($wrapped AS $wrap)
+			{
+				$result = array_merge($result, $this->getTables($moduleDir, $wrap));
+			}
+		}
+		elseif ($tableName = $config->get('table_name'))
+		{
+			$result[] = $this->rh->db_prefix.$tableName;
+		}
+		 
+		return array_unique($result);
+	}
+		
 	protected function getModulesList()
 	{
 		return $this->rh->db->query("
 			SELECT * 
 			FROM ??toolbar
-			WHERE LENGTH(href) > 0
+			WHERE LENGTH(href) > 0 AND _state IN (0,1)
 		");
 	}
 	
