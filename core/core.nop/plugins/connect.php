39e50513b10ff734caae7096c367708a61a0f38d
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-09-02 16:32:31 +0000

    * js && css compression
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@755 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/connect.php
+++ core/core.nop/plugins/connect.php
@@ -47,70 +47,74 @@ if ( $compile )
 	if ( isset($tpl->CONNECT[$compile]) && is_array($tpl->CONNECT[$compile]) )
 	{
 		$template = "_/connect.html:".$compile;
-
-		if (1 == 2 AND ("js" == $compile && Config::get('compress_js')) || ("css" == $compile && Config::get('compress_css')))
+		
+		if (("js" == $compile && Config::get('compress_js')) || ("css" == $compile && Config::get('compress_css')))
 		{
-//			$compressedName = '';
-//			foreach ($tpl->CONNECT[$compile] AS $fileName)
-//			{
-//				if (file_exists($tpl->getSkinDir().'/'.$compile.'/'.$fileName.'.'.$compile))
-//				{
-//					$compressedName .= '|'.filemtime($tpl->getSkinDir().'/'.$compile.'/'.$fileName.'.'.$compile).'|'.$fileName.'|';
-//				}
-//			}
-//			$compressedName = md5($compressedName);
-//			
-//			if (!file_exists(Config::get('cache_dir').'/'.$compile.'/'.$compressedName.'.'.$compile))
-//			{
-//				$result = '';
-//				
-//				foreach ($tpl->CONNECT[$compile] AS $filename)
-//				{
-//					if (file_exists($rh->app_dir.'skins/'.$rh->tpl_skin.'/'.$compile.'/'.$filename.'.'.$compile))
-//					{
-//						$result .= file_get_contents($rh->app_dir.'skins/'.$rh->tpl_skin.'/'.$compile.'/'.$filename.'.'.$compile);
-//					}
-//				}
-//				
-//				if ('js' == $compile)
-//				{
-//					$result = JSMin::minify($result);
-//				}
-//				elseif ('css' == $compile)
-//				{
-//					$result = CSSMin::minify($result);
-//				}
-//								
-//				if (!is_dir($rh->project_dir.'cache/'.$rh->app_name.'/'.$compile))
-//				{
-//					mkdir($rh->project_dir.'cache/'.$rh->app_name.'/'.$compile, 0775, true);
-//					$htaccess = "RewriteEngine on\r\n";
-//					if ('js' == $compile)
-//					{
-//						$htaccess .= "AddType application/x-javascript .gz\r\n";
-//						$htaccess .= "AddType application/x-javascript .js\r\n";
-//					}
-//					elseif ('css' == $compile)
-//					{
-//						$htaccess .= "AddType text/css .gz\r\n";
-//						$htaccess .= "AddType text/css .css\r\n";
-//					}
-//					$htaccess .= "RewriteRule ^(.*\.gz)$ $1 [L]\r\n";
-//					$htaccess .= "RewriteCond %{HTTP:Accept-Encoding} gzip\r\n";
-//					$htaccess .= "RewriteRule ^(.*\.".$compile.")$ $1.gz\r\n";
-//					$htaccess .= "AddEncoding gzip .gz\r\n";
-//					$htaccess .= "Header set ExpiresActive On\r\n";
-//					$htaccess .= "Header set ExpiresDefault \"access plus 10 years\"\r\n";
-//					file_put_contents($rh->project_dir.'cache/'.$rh->app_name.'/'.$compile.'/.htaccess', $htaccess);
-//				}
-//				
-//				file_put_contents($rh->project_dir.'cache/'.$rh->app_name.'/'.$compile.'/'.$compressedName.'.'.$compile, $result);
-//				file_put_contents($rh->project_dir.'cache/'.$rh->app_name.'/'.$compile.'/'.$compressedName.'.'.$compile.'.gz', gzencode($result, 9));
-//			}
-//			
-//			$fname = array('path' => $rh->base_url.'cache/'.$rh->app_name.'/'.$compile, 'file' => $compressedName);
-//			$tpl->set("*",$fname);
-//			$str = $tpl->parse($template."_path");
+			$compressedName = '';
+			foreach ($tpl->CONNECT[$compile] AS $fileName)
+			{
+				if (file_exists($tpl->getSkinDir().'/'.$compile.'/'.$fileName.'.'.$compile))
+				{
+					$compressedName .= '|'.filemtime($tpl->getSkinDir().'/'.$compile.'/'.$fileName.'.'.$compile).'|'.$fileName.'|';
+				}
+			}
+			$compressedName = md5($compressedName);
+			
+			if (!file_exists(Config::get('cache_dir').'/'.$compile.'/'.$compressedName.'.'.$compile))
+			{
+				$result = '';
+				$skinDir = $tpl->getSkinDir();
+				foreach ($tpl->CONNECT[$compile] AS $filename)
+				{
+					if (file_exists($skinDir.'/'.$compile.'/'.$filename.'.'.$compile))
+					{
+						$result .= file_get_contents($skinDir.'/'.$compile.'/'.$filename.'.'.$compile);
+					}
+				}
+				
+				if ('js' == $compile)
+				{
+					Finder::useClass('JSMin');
+					$result = JSMin::minify($result);
+				}
+				elseif ('css' == $compile)
+				{
+					Finder::useClass('CSSMin');
+					$result = CSSMin::minify($result);
+				}
+								
+				if (!is_dir(Config::get('cache_dir').'/'.$compile))
+				{
+					mkdir(Config::get('cache_dir').'/'.$compile, 0775, true);
+					$htaccess = "RewriteEngine on\r\nRewriteBase ".RequestInfo::$baseUrl.'cache/'.Config::get('app_name').'/'.$compile."\r\n";
+					if ('js' == $compile)
+					{
+						$htaccess .= "AddType application/x-javascript .gz\r\n";
+						$htaccess .= "AddType application/x-javascript .js\r\n";
+					}
+					elseif ('css' == $compile)
+					{
+						$htaccess .= "AddType text/css .gz\r\n";
+						$htaccess .= "AddType text/css .css\r\n";
+					}
+					$htaccess .= "RewriteRule ^(.*\.gz)$ $1 [L]\r\n";
+					$htaccess .= "RewriteCond %{HTTP:Accept-Encoding} gzip\r\n";
+					$htaccess .= "RewriteCond %{HTTP_USER_AGENT} !Safari\r\n";
+					$htaccess .= "RewriteCond %{HTTP_USER_AGENT} !Konqueror\r\n";
+					$htaccess .= "RewriteRule ^(.*\.".$compile.")$ $1.gz\r\n";
+					$htaccess .= "AddEncoding gzip .gz\r\n";
+					$htaccess .= "Header set ExpiresActive On\r\n";
+					$htaccess .= "Header set ExpiresDefault \"access plus 10 years\"\r\n";
+					file_put_contents(Config::get('cache_dir').'/'.$compile.'/.htaccess', $htaccess);
+				}
+				
+				file_put_contents(Config::get('cache_dir').'/'.$compile.'/'.$compressedName.'.'.$compile, $result);
+				file_put_contents(Config::get('cache_dir').'/'.$compile.'/'.$compressedName.'.'.$compile.'.gz', gzencode($result, 9));
+			}
+			
+			$fname = array('path' => RequestInfo::$baseUrl.'cache/'.Config::get('app_name').'/'.$compile, 'file' => $compressedName);
+			$tpl->set("*",$fname);
+			$str = $tpl->parse($template."_path");
 		}
 		else
 		{
