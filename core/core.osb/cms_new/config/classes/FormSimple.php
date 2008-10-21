da65514e853f7c0e1c69b04a112c684ffb9f80f0
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-10-21 12:57:03 +0000

    * CIFT-460
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@796 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.osb/cms_new/config/classes/FormSimple.php
+++ core/core.osb/cms_new/config/classes/FormSimple.php
@@ -1,15 +1,15 @@
 <?
-  
+
   $this->UseClass("DBDataEdit");
-  
+
 class FormSimple extends DBDataEdit  {
-  
+
   var $rh; //ссылка на $rh
   var $config; //ссылка на объект класса ModuleConfig
   var $loaded = false; //грузили или нет данные?
-  
+
   var $state; //персональный StateSet
-  
+
   //templates
   var $template = "form_simple.html";
   var $_template_item = 'form.html'; //basename шаблона формы, если брать его из конфига
@@ -17,22 +17,24 @@ class FormSimple extends DBDataEdit  {
   var $template_item = ''; //возьмём из конфига
   var $store_to;
   var $full_path = false; //брать шаблоны не из стандартной директории?
-  
+
   var $id_get_var = 'id';
   var $id_field = "id";
   var $id = 0; //id редактируемой записи
-  
+
   var $supertag_limit = 20;
   var $created_field = "_created";
-  
+
   function FormSimple( &$config )
   {
     //base modules binds
     $this->config =& $config;
-    
+
+    if ($this->config->id_field) $this->id_field = $this->config->id_field;
+
     //DBDataEdit
 //    $config->Read("form");
-    $config->SELECT_FIELDS[] = '_state';
+    if ($config->state_field) $config->SELECT_FIELDS[] = $config->state_field; else $config->SELECT_FIELDS[] = '_state';
     DBDataEdit::DBDataEdit( $config->rh, $config->table_name, $config->SELECT_FIELDS, $config->where, $config->order_by, $config->limit, $config->UPDATE_FIELDS );
     $this->prefix = $config->module_name.'_form_';
     $this->new_suffix = "";
@@ -48,15 +50,15 @@ class FormSimple extends DBDataEdit  {
     //StateSet
     $this->state =& new StateSet($this->rh);
     $this->state->Set($this->rh->state);
-    
+
     $this->rh->trash->table_id_field = $config->SELECT_FIELDS[0];
   }
-  
+
   function Load(){
     if( !$this->loaded ){
       if(!$this->id)
         $this->id = $this->state->Keep( $this->id_get_var, 'integer' );
-      
+
       DBDataEdit::Load( $this->SELECT_FIELDS[0]."='".$this->id."'" );
       if ($this->item['_state']>0)
           $this->rh->tpl->assign("body_class", "class='state1'");
@@ -65,28 +67,28 @@ class FormSimple extends DBDataEdit  {
     }
     return false;
   }
-  
+
   function Handle(){
-    
+
     //load data
     if( !$this->loaded ) $this->Load();
-    
+
     //куда делать редирект?
     $mode = $this->rh->GetVar('mode');
     $this->_redirect = $this->rh->url.'do/'.$this->config->module_name.( $mode ? '/'.$mode : '' ).'?'.$this->state->State();
     if( $this->rh->GetVar('update_2') )
       $this->_redirect .= '&update_2=1';
-    
+
     if(!$this->rh->GLOBALS[$this->prefix.'_state'])
     {
     	$this->rh->GLOBALS[$this->prefix.'_state'] = 0 ;
     }
-    
+
     //update data
     $redirect = $this->Delete();
     if(!$redirect) $redirect = $this->Restore();
     if(!$redirect) $redirect = $this->Update();
-    
+
     //редирект или выставление флага, что он нужен
     if($redirect){
 //      $redirect = $this->rh->url.'do/'.$this->config->module_name.'?'.$this->state->State();
@@ -95,22 +97,22 @@ class FormSimple extends DBDataEdit  {
         return;
       }else $this->rh->Redirect( $this->_redirect );
     }
-    
+
     $tpl =& $this->rh->tpl;
-    
+
     //подготовка нетекстовых полей
     $this->RenderFields();
-    
+
     //назначаем движок сохранения формы без перезагрузки
     if( !$rh->__form_loader ){
       $tpl->Parse( $this->template_loader, 'html_head', true );
       $rh->__form_loader = true;
     }
-    
+
     //обновляем родительское окно
     if( $this->rh->GetVar('update_2') && $this->rh->GetVar('popup') )
       $tpl->Parse( $this->template.':parent_reload', 'parent_reload' );
-    
+
     //render form
     $item = (object)$this->item;
     $id_field = $this->SELECT_FIELDS[0];
@@ -118,61 +120,61 @@ class FormSimple extends DBDataEdit  {
       $tpl->AssignRef( '*', $this->config->new );
     else
       $tpl->AssignRef( '*', $item );
-    
+
     $tpl->Assign( 'prefix', $this->prefix );
     $tpl->Assign( 'POST_STATE', $this->state->State(1) );
     $tpl->Assign( '__form_name', $this->prefix.'_simple_form' );
     $tpl->Assign( '__delete_title', $item->_state!=2 ? 'удалить в корзину' : 'удалить окончательно'  );
-    
+
     $tpl->Parse( $this->template_item, '___form', false, $this->full_path );
     if($this->id && !$this->config->hide_delete_button )
       $tpl->Parse( $this->template.':delete_button', '_delete_button' );
-    
+
     if(!$this->config->hide_save_button )
     {
     	if($item->_state == 2)
     	{
     		$tpl->Parse( $this->template.':restore_button', '_save_button' );
     	}
-    	else 
+    	else
     	{
       		$tpl->Parse( $this->template.':save_button', '_save_button' );
     	}
     }
-    
+
     if($this->config->send_button && $item->id  && $item->_state == 0)
     {
     	if($item->sended)
     	{
     		$tpl->Parse( $this->template.':send_button_disabled', '_send_button' );
     	}
-    	else 
+    	else
     	{
     		$tpl->Parse( $this->template.':send_button', '_send_button' );
     	}
     }
-    
+
     if(($this->config->save_button_norefresh ))
       $tpl->Parse( $this->template.':save_button_norefresh', '_save_button' );
-    
+
     $tpl->Parse( $this->template, $this->store_to, true );
-    
+
     //ссылка на просмотр логов
     if($this->id)
       $this->rh->logs->ParseLink( $this->config->module_title, $this->id, $this->store_to, true );
   }
-  
+
   function RenderFields(){
     if( $this->_fields_rendered ) return;
     $this->_fields_rendered = true;
-    
+
     $tpl =& $this->rh->tpl;
 /*
   $this->config->RENDER - каждая запись в нём:
     0 - имя поля
     1 - тип поля - checkbox | select | radiobutton
     2 - хэш значений - array( id => value )
-*/  
+*/
     if( is_array($this->config->RENDER) ){
       $N = count($this->config->RENDER);
       for($i=0;$i<$N;$i++){
@@ -188,7 +190,7 @@ class FormSimple extends DBDataEdit  {
             {
 //            modified by geronimo
 //              $_str .= "<option value='".$_id."' ".( $this->item[$row[0]]==$_id  || (!$this->item["id"] && $_id==$row[3]) ? "selected=\"selected\"" : '' ).">".$_val;
-              $_str .= "<option value='".$_id."' ".(($this->item["id"] && $this->item[$row[0]]==$_id) || (!$this->item["id"] && $_id==$row[3]) ? "selected=\"selected\"" : '' ).">".$_val;
+              $_str .= "<option value='".$_id."' ".(($this->item[$this->id_field] && $this->item[$row[0]]==$_id) || (!$this->item[$this->id_field] && $_id==$row[3]) ? "selected=\"selected\"" : '' ).">".$_val;
             }
             $tpl->Assign( 'options_'.$row[0], $_str );
           break;
@@ -199,28 +201,28 @@ class FormSimple extends DBDataEdit  {
       }
     }
   }
-  
+
   function Delete(){
     if( $this->rh->GetVar( $this->prefix."delete" ) )
     {
       $this->rh->logs->Put( 'Форма: '.($this->item['_state']<=1 ? 'удаление в корзину' : 'окончательное удаление'), $this->id, $this->config->module_title, $this->item[$this->SELECT_FIELDS[1]], $this->_redirect.'&_show_trash=1' );
       return $this->rh->trash->Delete( $this->config->table_name, $this->id, $this->config->module_title, $this->item[ $this->SELECT_FIELDS[1] ], $this->rh->path_rel.'?'.str_replace('&amp;','&',$this->state->StateAll()), $this->SELECT_FIELDS[0] );
-    }else 
+    }else
     {
         return false;
     }
   }
-  
+
   function Restore(){
     if( $this->rh->GetVar( $this->prefix."restore" ) )
     {
       return $this->rh->trash->FromTrash( $this->config->table_name, $this->id);
-    }else 
+    }else
     {
         return false;
     }
   }
-  
+
   function Update(){
     if( $this->needUpdate() )
     {
@@ -238,25 +240,25 @@ class FormSimple extends DBDataEdit  {
       }
       return true;
     }
-    else 
+    else
     {
         return false;
     }
   }
-  
+
   function needUpdate()
   {
   	return $this->rh->GetVar( $this->prefix."update" ) ? true : false;
-  } 
-  
+  }
+
   function _Filters( $suffix='' ){
     $rh =& $this->rh;
     $tpl =& $rh->tpl;
     $GLOBALS1 =& $rh->GLOBALS;
-    
+
     if($suffix=='')
       $suffix = $this->suffix;
-    
+
     //filter data
     if( is_array($this->config->UPDATE_FILTERS) ){
       foreach( $this->config->UPDATE_FILTERS as $field=>$filter ){
@@ -274,7 +276,7 @@ class FormSimple extends DBDataEdit  {
         }
       }
     }
-    
+
     //pre-rendering
     if( is_array($this->config->PRE_FILTERS) ){
       foreach( $this->config->PRE_FILTERS as $filter=>$FIELDS ){
@@ -288,7 +290,7 @@ class FormSimple extends DBDataEdit  {
         }
       }
     }
-    
+
     //supertag
     if( $this->config->supertag ){
       if( is_array($this->config->supertag) ){
@@ -314,7 +316,7 @@ class FormSimple extends DBDataEdit  {
       $this->UPDATE_FIELDS[] = '_supertag';
     }
   }
-  
+
   function AddNew(){
     //add new
     $id = DBDataEdit::AddNew( $this->config->INSERT_FIELDS );
@@ -324,5 +326,5 @@ class FormSimple extends DBDataEdit  {
     return $id;
   }
 }
-  
+
 ?>
