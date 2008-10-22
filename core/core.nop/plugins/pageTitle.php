1a93ffcca025ce58809447ee4fb322c8df0b3aa3
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-10-22 09:44:06 +0000

    * QUICKSTART-339
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@800 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/pageTitle.php
+++ core/core.nop/plugins/pageTitle.php
@@ -4,14 +4,11 @@ if (!Locator::exists('controller')) return;
 $controller = Locator::get('controller');
 if($controller['meta_title'])
 {
-	echo $controller['meta_title'];
+	echo strip_tags($controller['meta_title']);
 }
 else
 {
-	if($controller['title_short'])
-	{
-		echo strip_tags($controller['title_short']).' &mdash; ';
-	}
-	echo Config::get('project_title');
+	echo strip_tags($controller['title']);
+	//echo Config::get('project_title');
 }
 ?>
