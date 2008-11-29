059581f434cb766329f5efb78c3d6cd30cae3cc5
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-11-29 11:32:56 +0000

    * tweaks
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@866 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/plugins/dump.php
+++ core/core.nop/plugins/dump.php
@@ -3,7 +3,11 @@ $item = $params['item']?$params['item']:( $params['_'] ? $params['_'] : $params[
 
 echo '<pre>';
 if (is_object($item) && get_class($item)=='ResultSet')
-	echo $item;
+    echo $item;
+//      print_r($item->getData());
+
+elseif ( is_object($item) && get_class($item)=='DBModel' ) 
+    print_r($item->getData());
 else 
 	print_r($item);
 echo '</pre>';
