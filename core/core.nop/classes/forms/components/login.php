d4779cad4fb39a3789775768bf940cac45156dc4
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2008-12-10 10:14:12 +0000

    * HATTRICK-45
    git-svn-id: svn://svn.jetstyle.ru/core/branch/04@896 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core/core.nop/classes/forms/components/login.php
+++ core/core.nop/classes/forms/components/login.php
@@ -7,18 +7,20 @@ class FormComponent_login extends FormComponent_validator_base
 	function Validate()
 	{
 		FormComponent_validator_base::Validate();
-		
-		if (!$this->valid) return $this->valid;
-		
-		$value = $this->field->model->Model_GetDataValue();
-		
-		$model = clone Locator::get('principal')->getStorageModel();
-		$model->loadByLogin($value);
 
-		if ($model->getId())
-		{
-			$this->_Invalidate( "duplicate_login", "Выбранный логин уже занят" );
-			$this->valid = false;
+		if (!$this->valid) return $this->valid;
+
+		if($this->field->config['validator_params']['not_exists'])
+		{        	$value = $this->field->model->Model_GetDataValue();
+
+			$model = clone Locator::get('principal')->getStorageModel();
+			$model->loadByLogin($value);
+
+			if ($model->getId() && Locator::get('principal')->getId() != $model->getId())
+			{
+				$this->_Invalidate( "duplicate_login", "Выбранный логин уже занят" );
+				$this->valid = false;
+			}
 		}
 
 		return $this->valid;
