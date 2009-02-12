8f4ed71eab60e6132f587b2b2eba93042f0790df
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2009-02-12 10:06:36 +0000

    * QUICKSTART-163
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@1060 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.cms/classes/ListDrugs.php
+++ core.cms/classes/ListDrugs.php
@@ -24,22 +24,62 @@ class ListDrugs extends ListSimple
 
  function handle()
  {
- 	if (RequestInfo::get('order_list')) {
+ 	if (RequestInfo::get('order_list'))
+ 	{
  		$db = &Locator::get('db');
-    	$orders = explode(",",$_POST['order']) ;
-
+    	$orders = explode(",",$_POST['order_items']) ;
 	    foreach ($orders as $i=>$order)
 	    {
-	    	//$out .= "$order = $i \n\r";
 	        $sql = "UPDATE ??".$this->config->table_name." SET _order=".$db->quote($i)." WHERE id=".$db->quote($order);
 	        $db->execute($sql);
 	    }
 	    die('1');
  	}
+ 	if ($_GET['delete_list'])
+ 	{
+ 		$db = &Locator::get('db');
+        $items = $this->loadItems($_GET['items_list']);
+        $deleteItems = $updateItems = array();
+	    foreach ($items as $i=>$item)
+	    	if ($item['_state'] == 2)            	$deleteItems[] = $item['id'];
+	    	else                $updateItems[] = $item['id'];
+      	if (!empty($deleteItems))
+      		$db->execute("DELETE FROM ??".$this->config->table_name." WHERE id IN (".implode(',',$deleteItems).")");
+      	if (!empty($updateItems))
+      		$db->execute("UPDATE ??".$this->config->table_name." SET _state = _state + 1  WHERE id IN (".implode(',',$updateItems).")");
+      	Controller::redirect(RequestInfo::hrefChange('',array('delete_list'=>'', 'items_list'=>'')));
+ 	}
+ 	if ($_GET['restore_list'])
+ 	{
+ 		$db = &Locator::get('db');
+        $items = $this->loadItems($_GET['items_list']);
+        $updateItems = array();
+	    foreach ($items as $i=>$item)
+			if ($item['_state'] > 0) $updateItems[] = $item['id'];
+      	if (!empty($updateItems))
+      		$db->execute("UPDATE ??".$this->config->table_name." SET _state = _state - 1  WHERE id IN (".implode(',',$updateItems).")");
+      	Controller::redirect(RequestInfo::hrefChange('',array('restore_list'=>'', 'items_list'=>'')));
+ 	}
  	$tpl = &Locator::get('tpl');
     $tpl->set('page_url', RequestInfo::$baseUrl.RequestInfo::$pageUrl);
+    $tpl->set('group_delete_url', RequestInfo::hrefChange('',array('delete_list'=>'1')));
+    $tpl->set('group_restore_url', RequestInfo::hrefChange('',array('restore_list'=>'1')));
+    $tpl->set('group_operations', $this->config->group_operations);
     parent::Handle();
  }
+
+	function loadItems($items) {		$db = &Locator::get('db');
+		$items = explode(',',$items);
+		foreach ($items as &$item) {        	$item = intval($item);
+		}
+		$items = implode(',',$items);
+    	$items = $db->query("
+    		SELECT id, _state
+    		FROM ??".$this->config->table_name."
+    		WHERE id IN (".$items.")"
+    	);
+    	return $items;
+	}
 }
 
 ?>
