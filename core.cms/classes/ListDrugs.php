20209a614b2c2a72991b5b3396c7dc4db810ffbb
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2009-02-17 08:51:51 +0000

    * QUICKSTART-180
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@1071 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.cms/classes/ListDrugs.php
+++ core.cms/classes/ListDrugs.php
@@ -24,16 +24,46 @@ class ListDrugs extends ListSimple
 
  function handle()
  {
- 	if (RequestInfo::get('order_list'))
+ 	if ($_GET['order_list'] || $_POST['order_list'])
  	{
- 		$db = &Locator::get('db');
-    	$orders = explode(",",$_POST['order_items']) ;
-	    foreach ($orders as $i=>$order)
-	    {
-	        $sql = "UPDATE ??".$this->config->table_name." SET _order=".$db->quote($i)." WHERE id=".$db->quote($order);
-	        $db->execute($sql);
-	    }
-	    die('1');
+ 		$itemId = intval($_GET['item_id'] ? $_GET['item_id'] : $_POST['item_id']);
+ 		$page = intval($_GET[$this->pageVar] ? $_GET[$this->pageVar] : $_POST[$this->pageVar]);
+ 		$destIndex = intval($_GET['index'] ? $_GET['index'] : $_POST['index']) + ($page - 1) * $this->perPage;
+
+ 	    $db = &Locator::get('db');
+ 		$destItem = $db->query(
+ 			"SELECT _order FROM ??".$this->config->table_name." ORDER BY _order LIMIT 1 OFFSET ".$destIndex
+ 		);
+ 		if (!$destItem[0]['_order']) {        	$destItem = $db->query(
+	 			"SELECT _order FROM ??".$this->config->table_name." ORDER BY _order DESC LIMIT 1"
+	 		);
+ 		}
+ 		$destOrder = $destItem[0]['_order'];
+ 		$sourceItem = $db->queryOne(
+ 			"SELECT _order FROM ??".$this->config->table_name." WHERE id = ".$itemId
+ 		);
+ 		$sourceOrder = $sourceItem['_order'];
+ 		if ($sourceOrder > $destOrder)
+        	$db->execute("
+        		UPDATE ??".$this->config->table_name."
+        		SET _order = _order + 1
+        		WHERE _order < ".$sourceOrder." AND _order >= ".$destOrder
+        	);
+ 		else
+ 			$db->execute("
+        		UPDATE ??".$this->config->table_name."
+        		SET _order = _order - 1
+        		WHERE _order > ".$sourceOrder." AND _order <= ".$destOrder
+        	);
+        $db->execute("
+        	UPDATE ??".$this->config->table_name."
+        	SET _order = ".$destOrder."
+        	WHERE id = ".$itemId
+        );
+        if ($_POST['ajax_load'])
+        	die('1');
+        else
+        	Controller::redirect(RequestInfo::hrefChange('',array('id'=>$itemId, 'order_list'=>'', 'items_id'=>'', 'index'=>'')));
  	}
  	if ($_GET['delete_list'])
  	{
@@ -60,8 +90,22 @@ class ListDrugs extends ListSimple
       		$db->execute("UPDATE ??".$this->config->table_name." SET _state = _state - 1  WHERE id IN (".implode(',',$updateItems).")");
       	Controller::redirect(RequestInfo::hrefChange('',array('restore_list'=>'', 'items_list'=>'')));
  	}
+ 	/*if ($_GET['move_id'])
+ 	{
+ 		$db = &Locator::get('db');
+        $items = $this->loadItems($_GET['items_list']);
+        $updateItems = array();
+	    foreach ($items as $i=>$item)
+			if ($item['_state'] > 0) $updateItems[] = $item['id'];
+      	if (!empty($updateItems))
+      		$db->execute("UPDATE ??".$this->config->table_name." SET _state = _state - 1  WHERE id IN (".implode(',',$updateItems).")");
+      	Controller::redirect(RequestInfo::hrefChange('',array('move_id'=>'', 'id'=>intval($_GET['move_id']))));
+ 	}*/
  	$tpl = &Locator::get('tpl');
     $tpl->set('page_url', RequestInfo::$baseUrl.RequestInfo::$pageUrl);
+    $tpl->set('page_num', intval($_GET[$this->pageVar]));
+  	$tpl->set('page_var', $this->pageVar);
+  	$tpl->set('per_page', $this->perPage);
     $tpl->set('group_delete_url', RequestInfo::hrefChange('',array('delete_list'=>'1')));
     $tpl->set('group_restore_url', RequestInfo::hrefChange('',array('restore_list'=>'1')));
     $tpl->set('group_operations', $this->config->group_operations);
