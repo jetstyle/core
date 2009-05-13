2cbf16724cbbb5a168af28c7a096522cc166027f
lunatic <lunatic@fa5afafc-1ec3-db11-ae4b-00804824780f>
2009-05-13 04:49:05 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/trunk@1222 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/classes/controllers/ResponseController.php
+++ core.site/classes/controllers/ResponseController.php
@@ -30,7 +30,7 @@ class ResponseController extends Controller
 		Finder::useClass("forms/EasyForm");
 
         $formConfig = array();
-        $formConfig['action'] = Router::linkTo('Login').'?retpath='.$retPath;
+        $formConfig['action'] = RequestInfo::$baseUrl.Router::linkTo('Login').'?retpath='.$retPath;
 		$form =& new EasyForm('login', $formConfig);
 		
         Locator::get('tpl')->set('Form', $form->handle());
