f01d7756fdc971123e187e0883eeea39140d4310
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2009-05-27 13:36:12 +0000

    * QUICKSTART-873
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@1244 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/js/inplace.class.js
+++ core.site/js/inplace.class.js
@@ -22,8 +22,8 @@ Inplace.prototype =
 
     init: function()
     {
-	this.initContainer(true);
-	this.initButtons();
+		this.initContainer(true);
+		this.initButtons();
         this.bindAll();
 
     },
@@ -32,9 +32,9 @@ Inplace.prototype =
     {
 
     	//загрузим в редактор содержимое редактируемого контейнера
-	this.loadDataToInplaceEditor();
+		this.loadDataToInplaceEditor();
 
-	//unbind events from container
+		//unbind events from container
     	this.unbindAll();
     },
 
@@ -43,31 +43,21 @@ Inplace.prototype =
         this.showIndicator();
 
         if (this.cacheData )
-	{
+		{
             this.onLoad( this.cacheData );
-	}
+		}
         else
-	{
-		/*$(document).ajaxError(
-    			    function(event, request, settings)
-    			    {
-				this.hideIndicator();
-				onTBRemove = this.__get.prototypeBind(this);
-				tb_show("јвторизаци€", base_url+"cms/login/ajax?TB_iframe=true&width=264&height=90&onremove=true", "");
-    			    }.prototypeBind(this)
-		);*/
+		{
 	        this.__get();
-
-	}
+		}
     },
 
     __get: function ()
     {
-	    $.get(  this.cmsUrl,
-		            { 'ret': this.field },
-                    this.onLoad.prototypeBind(this), 'html'
-	        );
-
+	    $.get(this.cmsUrl,
+				{ 'ret': this.field },
+                this.onLoad.prototypeBind(this), 'html'
+	    );
     },
 
     onLoad: function (data)
@@ -78,20 +68,20 @@ Inplace.prototype =
 
             //–€дом с контейнером создаем его пустой клон, с инплейсным редактором
             //this.container.parent().append( this.inplaceContainer );
-	    this.container.after( this.inplaceContainer );
+			this.container.after( this.inplaceContainer );
         }
 
         //show inplaceObject and its container
         $( this.inplaceObject ).add( this.inplaceContainer ).removeClass("invisible");
 
-	//width fix
-	if (this.editorType!='wysiwyg')
-	    $( this.inplaceObject ).css( "width", $( this.inplaceObject ).children(":first").children(":first").width()-6 );
+		//width fix
+		if (this.editorType!='wysiwyg')
+		    $( this.inplaceObject ).css( "width", $( this.inplaceObject ).find('textarea').width()-6 );
 
         //контейнер спр€чем
         this.container.addClass("invisible");
 
-	// важно, что после манипул€ций с домом
+		// важно, что после манипул€ций с домом
         this.setEditorData(data);
 
         this.hideIndicator();
@@ -105,26 +95,16 @@ Inplace.prototype =
 	    }
 	    else
 	    {
-	        $( this.inplaceObject ).children(":first").children(":first").attr("value", data);
+	        $( this.inplaceObject ).find('textarea').attr("value", data);
 
 	        if (!this.inited)
-	        {	/*
-			for(id in tinyMCE.editors)
-			{
-			    console.log('Remove: ' + id );
-			    tinyMCE.execCommand('mceFocus', false, id );
-			    tinyMCE.execCommand('mceRemoveControl', false, id);
-			}
-			console.log(tinyMCE.editors);
-			*/
-			var textarea_id = $( this.inplaceObject ).children(":first").children(":first").attr("id");
-//			console.log('mceAddControl '+ textarea_id );
-
+	        {
+				var textarea_id = $( this.inplaceObject ).find("textarea").attr("id");
 		        tinyMCE.execCommand("mceAddControl", true, textarea_id );
 		        this.inited = true;
 	        }
-	        else
-		        tinyMCE.activeEditor.setContent( data );
+	        //else
+		    //    tinyMCE.activeEditor.setContent( data );
 	    }
     },
 
@@ -142,11 +122,11 @@ Inplace.prototype =
 
     showIndicator: function( before )
     {
-	this.container.removeClass("inplace-over");
+		this.container.removeClass("inplace-over");
 
-	if (before)
+		if (before)
             before.before( $("#cms_panel_loading").css('padding', "0 5px 0 0").css('display', '').removeClass('invisible') );
-	else
+		else
             this.container.append( $("#cms_panel_loading").css('padding', 5).css('display', 'block').removeClass('invisible') );
 
         this.saveButton.addClass("invisible");
@@ -155,8 +135,8 @@ Inplace.prototype =
 
     hideIndicator: function()
     {
-	//console.log( $("#cms_panel_loading") );
-	$("#cms_panel_loading").css('display', '').addClass('invisible');
+		//console.log( $("#cms_panel_loading") );
+		$("#cms_panel_loading").css('display', '').addClass('invisible');
         this.saveButton.removeClass("invisible");
         this.cancelButton.removeClass("invisible");
     },
@@ -175,7 +155,6 @@ Inplace.prototype =
 
     onSave: function(data)
     {
-
 	    this.container.html( data );
 	    this.cancel();
     },
@@ -208,63 +187,63 @@ Inplace.prototype =
 	    this.container.click( this.edit.prototypeBind( this ) );
 	    this.container.mouseover( function(){ $(this).addClass("inplace-over")    } );
 	    this.container.mouseout ( function(){ $(this).removeClass("inplace-over") } );
-
     },
 
     unbindAll: function()
     {
-	this.container.unbind('click');
+		this.container.unbind('click');
         this.container.unbind('mouseover');
         this.container.unbind('mouseout');
     },
 
     initContainer: function(parent)
     {
-	//контейнер инплейсного объекта
-	if ( parent )
-	    this.container = $( this.inplaceObject ).parent();
-	else
-	    this.container = $( this.inplaceObject ).prev();
+		//контейнер инплейсного объекта
+		if ( parent )
+			this.container = $( this.inplaceObject ).parent();
+		else
+			this.container = $( this.inplaceObject ).prev();
 
-	this.inplaceObject.css("z-index", 1000).css("position", "relative");
-	var dif = this.container.width() - this.inplaceObject.width();
-	if(dif){ this.inplaceObject.css("margin-left", dif); }
+		this.inplaceObject.css("z-index", 1000).css("position", "relative");
+		var dif = this.container.width() - this.inplaceObject.width();
+		//if(dif){ this.inplaceObject.css("margin-left", dif); }
 
-	if ( this.editorType == "wysiwyg" )
-	{
-	    textarea_id = this.inplaceObject.children(":first").attr("id");
+		if ( this.editorType == "wysiwyg" )
+		{
+			textarea_id = this.inplaceObject.children(":first").attr("id");
 
-	    //setTimeout( this.initMCE.prototypeBind(this, textarea_id),  100 );
-	    preInitMce(textarea_id);
-	}
+			//setTimeout( this.initMCE.prototypeBind(this, textarea_id),  100 );
+			preInitMce(textarea_id);
+		}
     },
 
     initButtons: function()
     {
-	//bind buttons
-	this.saveButton = $(document.createElement("input")).val("—охранить").attr("type", "button").addClass("cms-save-but hand");
-	this.cancelButton = $(document.createElement("input")).val("ќтменить").attr("type", "button").addClass("cms-delete-but hand");
+		//bind buttons
+		this.saveButton = $(document.createElement("input")).val("—охранить").attr("type", "button").addClass("cms-save-but hand");
+		this.cancelButton = $(document.createElement("input")).val("ќтменить").attr("type", "button").addClass("cms-delete-but hand");
 
-  this.buttons = $(document.createElement("div")).append( this.saveButton, this.cancelButton ).css("padding-top", "8px");//;
+		this.buttons = $(document.createElement("div")).append( this.saveButton, this.cancelButton ).css("padding-top", "8px");//;
 
-	if (this.editorType=="wysiwyg")
-	    $(this.buttons).css("padding-left", "8px");
-	else
-	    $(this.buttons).css("float", "right");
+		if (this.editorType=="wysiwyg")
+			$(this.buttons).css("padding-left", "8px");
+		else
+			$(this.buttons).css("float", "right");
 
-        this.saveButton.click(   this.save.prototypeBind(this)   );
-        this.cancelButton.click( this.cancel.prototypeBind(this) );
+		this.saveButton.click(   this.save.prototypeBind(this)   );
+		this.cancelButton.click( this.cancel.prototypeBind(this) );
 
-	//append buttons
-	this.inplaceObject.append( this.buttons );
+		//append buttons
+		this.inplaceObject.append( this.buttons );
     }
 
 }
 
 var mce_ids = [];
 var mce_timer = false
-    function preInitMce(textarea_id)
-    {
+
+function preInitMce(textarea_id)
+{
 	mce_ids.push(textarea_id);
 
 	if (!mce_timer)
@@ -272,69 +251,49 @@ var mce_timer = false
 	    setTimeout( initMCE, 1000 );
 	    mce_timer = true;
 	}
-    }
+}
 
-    function initMCE()
-    {
+function initMCE()
+{
 	var textarea_id = mce_ids;
-//	console.log('init please '+textarea_id);
 	var base_url = '/';
-//	var self = this;
+
 	tinyMCE.init({
 		mode : "none",
 		theme : "advanced",
 		elements: textarea_id,
 		language : "ru",
-    		plugin_preview_pageurl: base_url + "cms/preview",
-    		content_css: base_url + "cms/css/editor.css",
-    		plugin_preview_width: "400",
-    		button_tile_map : false,
-    		relative_urls : false,
+    	plugin_preview_pageurl: base_url + "cms/preview",
+    	content_css: base_url + "cms/css/editor.css",
+    	plugin_preview_width: "400",
+    	button_tile_map : false,
+    	relative_urls : false,
 
-    		plugins : "contextmenu,advlink,preview,jetimages",
+    	plugins : "contextmenu,advlink,preview,jetimages",
 
-    		valid_elements : "+a[name|href|target|title|onclick|mode|fileparams],-strong/-b/,-em/-i,-strike,-u,#p[id|style],-ol,-ul,-li,br,img[src|alt|title|bigwidth|bigheight|bigsrc|mode|width|height|title|style],-sub,-sup,-blockquote,-table[class],-tr[rowspan],tbody,thead,tfoot,#td[colspan|rowspan],-th[colspan|rowspan],caption,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr,dd,dl,dt,cite,abbr,acronym,del,ins,big",
-    		theme_advanced_toolbar_location : "top",
-    		theme_advanced_resizing : true,
-    		theme_advanced_statusbar_location : "false",
-    		theme_advanced_resize_horizontal : "false",
-    		theme_advanced_toolbar_align : "left",
-    		theme_advanced_buttons1 : "link,unlink,anchor,separator,justifyleft,justifycenter,justifyright,justifyfull,formatselect,bold,italic,underline,strikethrough,sub,sup,separator,bullist,numlist,separator,jetimages,jetfiles,code",
-    		theme_advanced_buttons2 : "",
-    		theme_advanced_buttons3 : "",
+    	valid_elements : "+a[name|href|target|title|onclick|mode|fileparams],-strong/-b/,-em/-i,-strike,-u,#p[id|style],-ol,-ul,-li,br,img[src|alt|title|bigwidth|bigheight|bigsrc|mode|width|height|title|style],-sub,-sup,-blockquote,-table[class],-tr[rowspan],tbody,thead,tfoot,#td[colspan|rowspan],-th[colspan|rowspan],caption,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr,dd,dl,dt,cite,abbr,acronym,del,ins,big",
+    	theme_advanced_toolbar_location : "top",
+    	theme_advanced_resizing : true,
+    	theme_advanced_statusbar_location : "false",
+    	theme_advanced_resize_horizontal : "false",
+    	theme_advanced_toolbar_align : "left",
+    	theme_advanced_buttons1 : "link,unlink,anchor,separator,justifyleft,justifycenter,justifyright,justifyfull,formatselect,bold,italic,underline,strikethrough,sub,sup,separator,bullist,numlist,separator,jetimages,jetfiles,code",
+    	theme_advanced_buttons2 : "",
+    	theme_advanced_buttons3 : "",
 		theme_advanced_blockformats : "p,h3,address",
-    		width: "100%",
-    		///height: "600px",
-
+    	width: "100%",
 
 		init_instance_callback: this.onMCEInit.prototypeBind(self)
-		});
+	});
 
 	tinyMCE.jetimages= base_url + "cms/do/Pictures/jetimages";
 	tinyMCE.jetfiles = base_url + "cms/do/PicFiles/jetfiles";
 	tinyMCE.jetcontent = base_url + "cms/jetcontent";
 
-    	tinyMCE.base_url = base_url + "cms/";
+    tinyMCE.base_url = base_url + "cms/";
+}
 
-//	this.mceInstance = tinyMCE;
-    	/*
-	$("div.col-2").ajaxError(function(event, request, settings)
-    	{
-    	   $(this).append("<span class='error'>Error requesting CMS. Please <a href='{{/}}cms/login'>login</a>.</span>");
-    	});
-	*/
-    };
-
-    function onMCEInit(inst)
-    {
-	    inst.resizeToContent();
-//    	    console.log('mce inited!!! '+inst.id);
-    	    //append buttons
-    	    //console.log( $(inst.getContainer()).width() );
-    	    //console.log( this );
-	    //<tr class="mceFirst"><td class="mceToolbar mceLeft mceFirst mceLast"></td></tr>
-	    //console.log( $(".mceLast") );
-    	    //$(inst.getContainer()).append( this.buttons );
-	    //$( this.buttons ).css("padding-left", "8px").css("float", "");
-	    //$( this.buttons ).css("width", $(inst.getContainer()).width() );
-    }
+function onMCEInit(inst)
+{
+	inst.resizeToContent();
+}
