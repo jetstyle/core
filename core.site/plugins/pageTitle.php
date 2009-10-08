eca9e87f38dee8c0539da3fa7cbd5734ce6574f4
bjornd <bjornd@fa5afafc-1ec3-db11-ae4b-00804824780f>
2009-10-08 04:21:44 +0000

    html_entities_decode for page title
    git-svn-id: svn://svn.jetstyle.ru/core/tags/05@1378 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/plugins/pageTitle.php
+++ core.site/plugins/pageTitle.php
@@ -4,11 +4,11 @@ if (!Locator::exists('controller')) return;
 $controller = Locator::get('controller');
 if($controller['meta_title'])
 {
-	echo strip_tags($controller['meta_title']);
+	echo html_entity_decode(strip_tags($controller['meta_title']));
 }
 else
 {
-	echo strip_tags($controller['title']);
+	echo html_entity_decode(strip_tags($controller['title']));
 	//echo Config::get('project_title');
 }
 ?>
