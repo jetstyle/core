3e1ad6ae885cce600a0cd4078af094f41bc218dd
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2010-02-08 12:02:27 +0000

    * fixed typo plugin
    git-svn-id: svn://svn.jetstyle.ru/core/trunk@1462 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/plugins/typografica.php
+++ core.site/plugins/typografica.php
@@ -1,6 +1,17 @@
 <?php
+/*
 if (!is_array($params)) $params = array("_"=>$params);
 $text = $params["_"]?$params["_"]:$params[0];
+
+
+*/
+
+if ( is_array($params) && $params["_"] ) 
+    $text = $params["_"];
+else if ( is_array($params) && $params[0] ) 
+    $text = $params[0];
+else
+    $text = $params;
 
 if ($text == "") return;
 
