f6230f0ef79aaf4a836392f83497d79f7c6d9443
vovan <vovan@fa5afafc-1ec3-db11-ae4b-00804824780f>
2010-09-13 11:40:17 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/tags/05@1727 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.cms/plugins/date_format.php
+++ core.cms/plugins/date_format.php
@@ -1,5 +1,6 @@
 <?php
 if (!$params['_']) $params['_'] = date('Y-m-d H:i:s');
 if (!$params['format']) $params['format'] = '%d.%m.%Y';
+$params['_'] = str_replace('0000-', date('Y') . '-', $params['_']);
 return strftime($params['format'], strtotime($params['_']));
 ?>
