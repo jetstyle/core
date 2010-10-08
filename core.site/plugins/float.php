fda52f65f38c62798617ba39ee6516b809433cbb
vovan <vovan@fa5afafc-1ec3-db11-ae4b-00804824780f>
2010-10-08 07:40:31 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/tags/06@1749 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/plugins/float.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!$params['_']) return false;
-$str = str_replace('.', ',', $params['_']);
-list($int, $div) = explode(',', $str);
-$int = number_format($int, 0, ',', ' ');
-$int = str_replace(' ', '&nbsp;', $int);
-if ($div) $int = $int . ',' . $div;
-return $int;
-?>
