75ab6c415d9637ed9081943a747607fab7455b81
vovan <vovan@fa5afafc-1ec3-db11-ae4b-00804824780f>
2010-11-23 12:37:54 +0000

    git-svn-id: svn://svn.jetstyle.ru/core/tags/09@1816 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.cms/classes/blocks/GotoMenuBlock.php
+++ core.cms/classes/blocks/GotoMenuBlock.php
@@ -4,13 +4,13 @@ class GotoMenuBlock extends Block
 {	
 	protected function constructData()
 	{
-                $pre_data = array();
-                $mod_config = Locator::get('controller')->moduleConstructor->getconfig();
-                if ( $mod_config["module_name"] == "Content" ){
-                    
-                    $pre_data[] = Locator::get('controller')->moduleConstructor->getForm()->getObj()->getItem()->getArray();
-                    $pre_data[] = array("separator"=>true);
-                }
+        $pre_data = array();
+        $mod_config = Locator::get('controller')->moduleConstructor->getconfig();
+        
+        if ( $mod_config["module_name"] == "Content" && $mod_config["module_path"] != 'Content/jetcontent' ){
+            $pre_data[] = Locator::get('controller')->moduleConstructor->getForm()->getObj()->getItem()->getArray();
+            $pre_data[] = array("separator"=>true);
+        }
                 
 		$data = Locator::get('db')->query("
  			SELECT IF(LENGTH(title_short) > 0, title_short, title_pre) AS title, _path
