d15e2f36fbffc1c6aaa9078f0dd7712009a7ade7
nop <nop@fa5afafc-1ec3-db11-ae4b-00804824780f>
2011-04-21 04:04:15 +0000

    + naumen phone
    git-svn-id: svn://svn.jetstyle.ru/core/tags/10@1920 fa5afafc-1ec3-db11-ae4b-00804824780f

--- core.site/classes/forms/components/group.php
+++ core.site/classes/forms/components/group.php
@@ -249,6 +249,7 @@ class FormComponent_group extends FormComponent_abstract
 
     function Interface_ParseEmail(  )
     {
+        $emailData[] = array("title"=>"<b>".$title."</b>");
         foreach ($this->field->model->childs as $field_in_group)
         {
             $title = $field_in_group->config["wrapper_title"];
